4ba72f9293bd96516a8d69b7b0b4355a788fcae3
Crypticant <aidan.undheim@gmail.com>
2021-12-28 17:05:02 -0700

    improve error handling for sqrt

--- sqrt.c
+++ sqrt.c
@@ -13,15 +13,25 @@ int main(int argc, char *argv[])
 	}
 	double input;
 	sscanf(argv[1], "%lf", &input);
+	if (input < 0) {
+		fputs("no\n", stderr);
+		exit(1);
+	}
 	printf("sqrt(%lf) ~ %lf\n", input, sqrt(input));
 }
 
 double ksqrt(double n, int k)
 {
+	if (n == 0) return 0;
 	double est = n;
 
 	int exp = ((*((long long int*)&n) >> 52) & 0x7ff) - 0x3ff;
-	est /= (2 << (((exp + 1) >> 1) - 1));
+	
+	if (exp == 0) {
+		est = 1;
+	} else {
+		est /= (2 << (((exp + 1) >> 1) - 1));
+	}
 
 	while (k-- > 0) {
 		est = (est + (n / est)) / 2;
