58586ba8b0eac8dbc09db5adca63f217c38b4501
Aidan Undheim <aidan.undhiem@gmail.com>
2023-09-04 22:44:42 -0700

    change algorithm to make it scale

--- sqrt.c
+++ sqrt.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define sqrt(n) ksqrt(n, 5)
+
+// hell hell hell hell trick c into letting me treat n as bits
+#define BITS(n) (*((unsigned long long int*)&n))
 
 double ksqrt(double n, int k);
 
@@ -17,24 +21,48 @@ int main(int argc, char *argv[])
 		fputs("no\n", stderr);
 		exit(1);
 	}
-	printf("sqrt(%lf) ~ %lf\n", input, sqrt(input));
+	printf("sqrt(%.17g) ~ %.17g\n", input, sqrt(input));
 }
 
 double ksqrt(double n, int k)
 {
 	if (n == 0) return 0;
-	double est = n;
+	double est = 1.5;
 
-	int exp = ((*((long long int*)&n) >> 52) & 0x7ff) - 0x3ff;
+	unsigned long long int temp;
+
+	int magicalFlag = 0;
+
+	int exp = ((BITS(n) >> 52) & 0x7ff);
+	if ((exp & 1) == 0) {
+		exp--;
+		magicalFlag = 1;
+		est = 3;
+	}
+	unsigned int newExp = (exp - 0x3ff) / 2 + 0x3ff;
+
+	// set exponent to 0
+	temp = (BITS(n) & 0x800fffffffffffff) | ((unsigned long long int)0x3ff << 52);
+	memcpy(&n, &temp, 8);
 	
-	if (exp == 0) {
-		est = 1;
-	} else {
-		est /= (2 << (((exp + 1) >> 1) - 1));
+	printf("%lf\n", n);
+
+	// magical stuff
+	if (magicalFlag) {
+		n *= 2;
 	}
 
-	while (k-- > 0) {
+	printf("%lf\n", n);
+
+	while (k --> 0) {
 		est = (est + (n / est)) / 2;
 	}
+	printf("%lf\n", est);
+
+	// re-add exponent
+	printf("%d\n", newExp);
+	temp = (BITS(est) & 0x800fffffffffffff) | ((unsigned long long int)(newExp) << 52);
+	memcpy(&est, &temp, 8);
+	printf("%lf\n", est);
 	return est;
 }
