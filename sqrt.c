74abc8762d19253ebdb3d287b866c5836dfd8925
Aidan Undheim <aidan.undhiem@gmail.com>
2023-09-05 09:17:30 -0700

    cleanup of sqrt

--- sqrt.c
+++ sqrt.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 #define sqrt(n) ksqrt(n, 5)
 
 // hell hell hell hell trick c into letting me treat n as bits
-#define BITS(n) (*((unsigned long long int*)&n))
+// god I shouldve used bit fields this sucks
+#define BITS(n) (*((uint64_t*)&n))
 
 double ksqrt(double n, int k);
 
 int main(int argc, char *argv[])
 {
+	double input;
+
 	if (argc < 2) {
 		fprintf(stderr, "Usage: %s <num>\n", argv[0]);
 		exit(1);
 	}
-	double input;
 	sscanf(argv[1], "%lf", &input);
+
 	if (input < 0) {
 		fputs("no\n", stderr);
 		exit(1);
@@ -26,12 +30,12 @@ int main(int argc, char *argv[])
 
 double ksqrt(double n, int k)
 {
-	if (n == 0) return 0;
-	double est = 1.5;
-
-	unsigned long long int temp;
-
+	uint64_t temp;
 	int magicalFlag = 0;
+	double est = 1.5;
+	unsigned int newExp;
+
+	if (n == 0) return 0;
 
 	int exp = ((BITS(n) >> 52) & 0x7ff);
 	if ((exp & 1) == 0) {
@@ -39,30 +43,24 @@ double ksqrt(double n, int k)
 		magicalFlag = 1;
 		est = 3;
 	}
-	unsigned int newExp = (exp - 0x3ff) / 2 + 0x3ff;
+	newExp = (exp - 0x3ff) / 2 + 0x3ff;
 
 	// set exponent to 0
-	temp = (BITS(n) & 0x800fffffffffffff) | ((unsigned long long int)0x3ff << 52);
+	temp = (BITS(n) & 0x800fffffffffffff) | ((uint64_t)0x3ff << 52);
 	memcpy(&n, &temp, 8);
 	
-	printf("%lf\n", n);
-
-	// magical stuff
+	// has to be after to not be put back into exponent
 	if (magicalFlag) {
 		n *= 2;
 	}
 
-	printf("%lf\n", n);
-
+	// funny -- thing
 	while (k --> 0) {
 		est = (est + (n / est)) / 2;
 	}
-	printf("%lf\n", est);
 
 	// re-add exponent
-	printf("%d\n", newExp);
-	temp = (BITS(est) & 0x800fffffffffffff) | ((unsigned long long int)(newExp) << 52);
+	temp = (BITS(est) & 0x800fffffffffffff) | ((uint64_t)(newExp) << 52);
 	memcpy(&est, &temp, 8);
-	printf("%lf\n", est);
 	return est;
 }
